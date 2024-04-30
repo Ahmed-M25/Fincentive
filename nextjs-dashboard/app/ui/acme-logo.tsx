d85bb1c5d5f692b2e7cbf69d601756f2f2905586
Ahmed <ahmedm25085@gmail.com>
2024-04-30 09:11:22 +0300

    implemented git dashboard

---
+++ nextjs-dashboard/app/ui/acme-logo.tsx
@@ -0,0 +1,13 @@
+import { CurrencyDollarIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <CurrencyDollarIcon className="h-12 w-12]" />
+      <p className="text-[44px]">Fincentive</p>
+    </div>
+  );
+}
