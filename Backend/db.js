99d88a90d10260f9c336bf4152a867a4a1d165da
Ahmed <ahmedm25085@gmail.com>
2024-07-10 19:25:20 +0200

    db.js setup

---
+++ Backend/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+  
+connection.connect((err) => {
+if (err) {
+    console.error('Error connecting to MySQL:', err.stack);
+    return;
+}
+console.log('Connected to MySQL as id', connection.threadId);
+});
+
+module.exports = connection;
--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,3 @@
-const mysql = require('mysql');
 const admin = require('firebase-admin');
 
 
@@ -8,20 +7,7 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err.stack);
-    return;
-  }
-  console.log('Connected to MySQL as id', connection.threadId);
-});
 
 
 
